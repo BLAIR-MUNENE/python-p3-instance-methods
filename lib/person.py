48bede1c3444bf0c22dfb77dd7185ecd3c86875f
blairtchalla821@gmail.com <blairtchalla821@gmail.com>
2024-06-13 11:34:28 +0300

    save work

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
-
-    #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+    
+    def walk(self):
+        print("This person is walking")
